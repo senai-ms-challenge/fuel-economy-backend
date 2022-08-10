ffae8754902e9f2f1ed4e5c0a1c681efd97b7463
Gabriel Menezes <gabrielufmscc@gmail.com>
2022-08-10 08:31:42 -0400

    Start Spring Boot project

---
+++ src/test/java/com/example/fueleconomybackend/FuelEconomyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fueleconomybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelEconomyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
