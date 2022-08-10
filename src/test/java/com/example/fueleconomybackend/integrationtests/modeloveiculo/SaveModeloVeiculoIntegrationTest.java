211a65adcec130c3b978004a79acb5f538be9a5d
Gabriel Menezes <gabrielufmscc@gmail.com>
2022-08-10 10:46:08 -0400

    US: Saving Modelo Veiculo Third TDD step: refactoring

--- src/test/java/com/example/fueleconomybackend/integrationtests/modeloveiculo/SaveModeloVeiculoIntegrationTest.java
+++ src/test/java/com/example/fueleconomybackend/integrationtests/modeloveiculo/SaveModeloVeiculoIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.example.fueleconomybackend.integrationtests.modeloveiculo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 
 @SpringBootTest
@@ -33,25 +35,37 @@ public class SaveModeloVeiculoIntegrationTest {
 
     @Test
     public void mustSaveModeloVeiculoSuccessfully() throws Exception {
+        HashMap<String, String> modeloVeiculo = createModeloVeiculoHashMap();
+
+        MockHttpServletResponse response = performPost(modeloVeiculo);
+
+        HashMap savedModeloVeiculo = mapToHashMap(response);
+
+        Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+        Assertions.assertNotNull(savedModeloVeiculo.get("id"));
+        Assertions.assertEquals(modeloVeiculo.get("nome"), savedModeloVeiculo.get("nome"));
+        Assertions.assertEquals(modeloVeiculo.get("fabricante"), savedModeloVeiculo.get("fabricante"));
+    }
+
+    private HashMap mapToHashMap(MockHttpServletResponse response) throws JsonProcessingException, UnsupportedEncodingException {
+        return this.mapper.readValue(response.getContentAsString(), HashMap.class);
+    }
+
+    private HashMap<String, String> createModeloVeiculoHashMap() {
         HashMap<String, String> modeloVeiculo = new HashMap<>();
         modeloVeiculo.put("nome", "HB20");
         modeloVeiculo.put("fabricante", "Hyundai");
+        return modeloVeiculo;
+    }
 
+    private MockHttpServletResponse performPost(HashMap<String, String> modeloVeiculo) throws Exception {
         String json = this.mapper.writeValueAsString(modeloVeiculo);
 
         MockHttpServletRequestBuilder request =
                 MockMvcRequestBuilders.post(PATH).contentType(MediaType.APPLICATION_JSON).content(json);
 
         MockHttpServletResponse response = this.mockMvc.perform(request).andReturn().getResponse();
-
-        HashMap savedModeloVeiculo = this.mapper.readValue(response.getContentAsString(), HashMap.class);
-
-        Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
-        Assertions.assertNotNull(savedModeloVeiculo.get("id"));
-        Assertions.assertEquals(modeloVeiculo.get("nome"), savedModeloVeiculo.get("nome"));
-        Assertions.assertEquals(modeloVeiculo.get("fabricante"), savedModeloVeiculo.get("fabricante"));
-
-
+        return response;
     }
 
 }
