f5544a89e5be2fc41e14280be379e7f509e56a13
Gabriel Menezes <gabrielufmscc@gmail.com>
2022-08-10 10:06:53 -0400

    US: Saving Modelo Veiculo First TDD step: test fails

--- src/test/java/com/example/fueleconomybackend/FuelEconomyBackendApplicationTests.java
+++ src/test/java/com/example/fueleconomybackend/FuelEconomyBackendApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.fueleconomybackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class FuelEconomyBackendApplicationTests {
 
     @Test
---
+++ src/test/java/com/example/fueleconomybackend/integrationtests/modeloveiculo/SaveModeloVeiculoIntegrationTest.java
@@ -0,0 +1,57 @@
+package com.example.fueleconomybackend.integrationtests.modeloveiculo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.HashMap;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+public class SaveModeloVeiculoIntegrationTest {
+
+    private static final String PATH = "/modelo-veiculos";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    public void mustSaveModeloVeiculoSuccessfully() throws Exception {
+        HashMap<String, String> modeloVeiculo = new HashMap<>();
+        modeloVeiculo.put("nome", "HB20");
+        modeloVeiculo.put("fabricante", "Hyundai");
+
+        String json = this.mapper.writeValueAsString(modeloVeiculo);
+
+        MockHttpServletRequestBuilder request =
+                MockMvcRequestBuilders.post(PATH).contentType(MediaType.APPLICATION_JSON).content(json);
+
+        MockHttpServletResponse response = this.mockMvc.perform(request).andReturn().getResponse();
+
+        HashMap savedModeloVeiculo = this.mapper.readValue(response.getContentAsString(), HashMap.class);
+
+        Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+        Assertions.assertNotNull(savedModeloVeiculo.get("id"));
+        Assertions.assertEquals(modeloVeiculo.get("nome"), savedModeloVeiculo.get("nome"));
+        Assertions.assertEquals(modeloVeiculo.get("fabricante"), savedModeloVeiculo.get("fabricante"));
+
+
+    }
+
+}
