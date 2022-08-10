852163d72b37428c67a1009f7c0ff112e485863d
Gabriel Menezes <gabrielufmscc@gmail.com>
2022-08-10 08:50:01 -0400

    Creating ModeloVeiculo entity

---
+++ src/main/java/com/example/fueleconomybackend/domain/ModeloVeiculo.java
@@ -0,0 +1,50 @@
+package com.example.fueleconomybackend.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class ModeloVeiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String fabricante;
+
+    public ModeloVeiculo() {
+    }
+
+    public ModeloVeiculo(Integer id, String name, String fabricante) {
+        this.id = id;
+        this.name = name;
+        this.fabricante = fabricante;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+}
